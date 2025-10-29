b4e1fd394055ed3b696644e69f96b164086862b9
Muhammad Asad <m.asad1856079@gmail.com>
2025-10-30 00:02:26 +0500

    added google analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,27 +3,38 @@ import "./globals.css";
 import Footer from "./_components/Footer";
 import NextTopLoader from "nextjs-toploader";
 import { CartProvider } from "@/context/CartContext";
+import Script from "next/script";
 
 export const metadata = {
-  title: "Ecommerce",
-  description:
-    "Discover most trending products here!",
+  title: "MedsOnline4u",
+  description: "Discover most trending products here!",
   keywords: "ecommerce",
 };
 
 export default function Layout({ children }) {
   return (
     <html lang="en">
-      <head />
+      <head>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-KRN5D84095"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-KRN5D84095');
+          `}
+        </Script>
+      </head>
       <body>
-      <NextTopLoader showSpinner={false} color="lab(56 -45.31 16.82)" />
-      <main className="min-h-screen bg-slate-50 text-slate-900 antialiased">
-        <Navbar />
-        <CartProvider>
-          {children}
-        </CartProvider>
-        <Footer />
-      </main>
+        <NextTopLoader showSpinner={false} color="lab(56 -45.31 16.82)" />
+        <main className="min-h-screen bg-slate-50 text-slate-900 antialiased">
+          <Navbar />
+          <CartProvider>{children}</CartProvider>
+          <Footer />
+        </main>
       </body>
     </html>
   );
